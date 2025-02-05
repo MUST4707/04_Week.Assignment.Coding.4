f8f3c9d7d109e5ebdfb18b0a3de2c3e80c5b8c4c
Sam Wells <32932600+sllewmas@users.noreply.github.com>
2025-02-04 22:06:52 -0500

    removed older export code

--- script.js
+++
@@ -1,7 +0,0 @@
-//↓↓↓↓↓↓↓↓↓↓↓↓YOUR CODE goes below here↓↓↓↓↓↓↓↓↓↓↓↓
-// Task 1: Variables Practice
-
-
-//↑↑↑↑↑↑↑↑↑↑YOUR CODE goes above here↑↑↑↑↑↑↑↑↑↑
-//Do not edit below this comment!
-module.exports = { myLetVariable, myConstVariable, myArray }; // Exporting for testing
